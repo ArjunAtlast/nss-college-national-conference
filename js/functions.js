1ca840bf2675cda83ebc3f038481f426ea341d97
Arjun <arjunatlast@gmail.com>
2017-07-10 23:25:23 +0530

    Basic HTML Skeleton Created

---
+++ js/functions.js
@@ -0,0 +1,5 @@
+/** Javascript File
+** @author NSSCE, Palakkad
+** @summary Includes all functions needed for the working of website
+** @requires https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
+**/
