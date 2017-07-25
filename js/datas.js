82e0855b0a4a07f93cef92d10ea2eacec2715307
Arjun <arjunatlast@gmail.com>
2017-07-25 11:44:36 +0530

    Added first set of datas

--- js/datas.js
+++ js/datas.js
@@ -19,29 +19,12 @@ var Data = {
 
   /*Accepted Papers */
   acceptedPapers: [
-    {
-      id: 2,
-      title: "Syllable Based Automatic speech recognition for Malayalam Using Deep Auto Encoder",
-      author: "Jini Cheriyan"
-    },
-    {
-      id: 4,
-      title: "Access Database Using Natural Language to Create SQL Query",
-      author: "Jitesh Prasad, Khatick"
-    },
-    {
-      id: 5,
-      title: "Normalized Hybrid Wavelet Features for Speech Emotion Recognition",
-      author: "Firoz Shah.A"
-    },
-    {
-      id: 6,
-      title: "Text Summarization Algorithms within Latent Semantic Analysis",
-      author: "Kanitha.D.K"
-    }
   ],
 
   keynoteSpeakers: [
+    /*
+    ## Example Item
+    ***************
     {
       id: 0,
       image: "./assets/images/avatar-placeholder.png",
@@ -52,16 +35,7 @@ var Data = {
         Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
         Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.`)
     },
+    */
 
-    {
-      id: 0,
-      image: "./assets/images/avatar-placeholder.png",
-      name: "Christopher Nolan",
-      detail: "Director",
-      bio: (`Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-        Aenean commodo ligula eget dolor. Aenean massa.
-        Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-        Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.`)
-    },
   ]
 };
